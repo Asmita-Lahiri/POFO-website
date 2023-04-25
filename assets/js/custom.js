e781bb9ba6ba4b64f504db6d4b0af3aa49ca4a11
Asmita <76862619+Asmita-Lahiri@users.noreply.github.com>
2023-04-25 11:12:47 +0530

    Added all files

---
+++ assets/js/custom.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+
+$('.slider_logos').slick({
+   dots: false,
+   slidesToShow: 4,
+})
+
+
+jQuery('.navigation').meanmenu();
+jQuery('.work_nav_holder').meanmenu();
+
+
+
+function chargebattery() {
+  var a;
+  a = document.getElementById("progress-bar");
+  a.innerHTML = "&#xf244;";
+  setTimeout(function () {
+      a.innerHTML = "&#xf243;";
+    }, 1000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf242;";
+    }, 2000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf241;";
+    }, 3000);
+  setTimeout(function () {
+      a.innerHTML = "&#xf240;";
+    }, 4000);
+}
+chargebattery();
+setInterval(chargebattery, 5000);
